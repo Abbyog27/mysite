42b0afe2c91cbd99d7d50532b33fab4e71ff82dc
Abigail Olaguivel <abbyog27@gmail.com>
2024-02-06 20:33:09 -0600

    feat: added part 3 of django app

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from pymongo import MongoClient
 import os 
 from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
 from bson import ObjectId
+from .models import Question
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
 
 client = MongoClient(os.getenv('MONGO_URI'))
 db = client['mysite']
@@ -66,5 +70,33 @@ def delete_question(question_id):
     db.polls_question.delete_one({"_id": ObjectId(question_id)})
     print('Question deleted')
     
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+

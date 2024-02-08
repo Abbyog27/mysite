44c5be62d462a1b8ea9981198aef4745a8c05826
Abigail Olaguivel <abbyog27@gmail.com>
2024-02-07 21:26:42 -0600

    chore: created requirements.txt

--- polls/views.py
+++ polls/views.py
@@ -35,42 +35,42 @@ db.polls_question.insert_one(question_two)
 print(db.polls_question.find_one())
 # print('new polls question', db.polls_question.find_one({'_id'}))
 
-#TODO Search by question_text
-def search_question_by_text(question_text):
-    for question in db.polls_question.find_all():
-        if question['question_text'] == question[question_text]:
-            print(question_text)
-            return question
-            print("question not found")
-            return None
+# #TODO Search by question_text
+# def search_question_by_text(question_text):
+#     for question in db.polls_question.find_all():
+#         if question['question_text'] == question[question_text]:
+#             print(question_text)
+#             return question
+#             print("question not found")
+#         return None
             
 
-#TODO Search a question by a certain text
-def search_question_by_text(question_text):
-    for question in db.polls_question.find_all():
-        if question_text in question['question_text']:
-            print('Found question')
-            return question
-            print("question not found")
-            return None
+# #TODO Search a question by a certain text
+# def search_question_by_text(question_text):
+#     for question in db.polls_question.find_all():
+#         if question_text in question['question_text']:
+#             print('Found question')
+#             return question
+#             print("question not found")
+#         return None
             
 
 
-#TODO Search all questions by one date
-def search_question_by_date(pub_date):
-    for question in db.polls_question.find():
-        if question['pub_date'] == pub_date: 
-            return question
+# #TODO Search all questions by one date
+# def search_question_by_date(pub_date):
+#     for question in db.polls_question.find():
+#         if question['pub_date'] == pub_date: 
+#             return question
 
-#TODO Update a question (pub_date -> needs to be set to current date)
-def update_question_date(question_id, new_pub_date):
-    db.polls_question.update_one({"_id": ObjectId(question_id)}, {"$set": {"pub_date": new_pub_date}})
-    print('Question has been updated')
+# #TODO Update a question (pub_date -> needs to be set to current date)
+# def update_question_date(question_id, new_pub_date):
+#     db.polls_question.update_one({"_id": ObjectId(question_id)}, {"$set": {"pub_date": new_pub_date}})
+#     print('Question has been updated')
     
-#TODO Delete a question
-def delete_question(question_id):
-    db.polls_question.delete_one({"_id": ObjectId(question_id)})
-    print('Question deleted')
+# #TODO Delete a question
+# def delete_question(question_id):
+#     db.polls_question.delete_one({"_id": ObjectId(question_id)})
+#     print('Question deleted')
     
 
 
@@ -99,10 +99,10 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
-        :5
-    ]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
     
+
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
